2b5b2a674ab455f8f0a380770e25c7186befd36f
ETMLNET\jerjeanelie <jeremiego03@gmail.com>
2023-12-14 08:24:25 +0100

    Corrections TE1

--- Exercice/Exo3/SOLUTION_NBN/Main.h
+++
@@ -1,9 +0,0 @@
-//////////////////////////////////////////
-// Titre : Ex_3
-// Nom : Besson Nicolas
-// Fichier : Main.h
-//////////////////////////////////////////
-
-#define VMAX 10000		// Constante 16 bit signée
-#define VMIN -10000		// Constante 16 bit signée
-#define PI 3.14159		// Constante reel 64 bit
