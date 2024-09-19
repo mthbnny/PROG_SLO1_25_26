93536358efaa6019994f5a782d86ca12213d2ab0
user <user@etml-es.ch>
2024-09-19 16:20:17 +0200

    demo slo1 code 1

---
+++ CodeDemo/codeDemoSLO24_25/demoSLO1_24_25/demoSLO1_24_25/demoSLO.c
@@ -0,0 +1,77 @@
+/*---------------------------------------------------------------------------------- -//
+// Nom du projet 		: demoSLO1_24_25
+// Nom du fichier 		: demoSLO.c 
+// Date de création 	: 19.09.2024
+// Date de modification : xx.xx.20xx
+//
+// Auteur 				: Philou (Ph. Bovey)
+//
+// Version				: 0.1
+//
+// Description          : demo pour les slos 1 
+//
+//
+// Remarques :            lien pour la table ASCII :
+// 						  -> http://www.asciitable.com/
+// 						  lien pour la saisie de clavier avec getc & getchar pour ne pas
+//                        avoir des erreurs d'interprétation
+// 						  -> http://fr.openclassrooms.com/informatique/cours/utiliser-les-bonnes-fonctions-d-entree
+// 						  lien vers différents
+// 						  -> http://fr.wikipedia.org/wiki/String.h
+//----------------------------------------------------------------------------------*/
+//-- appel librairies standard 
+#include <math.h>						// 
+#include <corecrt_math_defines.h>
+
+
+//-- appel de headerfile  -> vos fichier headerfile 
+
+ 
+
+//-- prototype - WARNING à évité ici 
+
+
+//-- variable globale - WARNING à évité 
+
+
+//-- CONSTANTE - WARNING à évité ici 
+
+
+
+//-- déclaration de fonction 
+
+
+//-- fonction principale -> main 
+// type_de_sortie nom_fct (param_entree1, param_entree2, ....)
+void main() 
+{   
+	//-- déclaration de variable --// 
+	// type nom_variable 
+	//type entier 
+	int longueur_m = 100, largeur_m = 10 ; 
+
+	//type reel 
+	float surfaceCarre_m2, surfaceRectangle_m2, surfaceTriangle_m2, surfaceCercle_m2; 
+
+
+
+
+
+	//instruction 1
+
+	//instruction 2
+
+	// test condtion 
+		// instruction 1A
+
+
+		// instruction 2A
+
+	//instruction 3
+
+
+}
+	
+
+
+
