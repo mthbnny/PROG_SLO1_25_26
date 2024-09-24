9f321221b9154407920b87d25ff650a1c409e1ff
user <user@etml-es.ch>
2024-09-24 17:03:59 +0200

    MAJ code Demo - calcul Surface - non approuvé

--- CodeDemo/codeDemoSLO24_25/demoSLO1_24_25/demoSLO1_24_25/demoSLO.c
+++ CodeDemo/codeDemoSLO24_25/demoSLO1_24_25/demoSLO1_24_25/demoSLO.c
@@ -2,13 +2,14 @@
 // Nom du projet 		: demoSLO1_24_25
 // Nom du fichier 		: demoSLO.c 
 // Date de création 	: 19.09.2024
-// Date de modification : xx.xx.20xx
+// Date de modification : 24.09.2024
 //
 // Auteur 				: Philou (Ph. Bovey)
 //
 // Version				: 0.1
 //
 // Description          : demo pour les slos 1 
+//						  1) calculer différentes surfaces 
 //
 //
 // Remarques :            lien pour la table ASCII :
@@ -19,58 +20,56 @@
 // 						  lien vers différents
 // 						  -> http://fr.wikipedia.org/wiki/String.h
 //----------------------------------------------------------------------------------*/
-//-- appel librairies standard 
-#include <math.h>						// 
-#include <corecrt_math_defines.h>
+//-- CONSTANTE - WARNING à évité ici 
+#define _USE_MATH_DEFINES				// pour utiliser certaines constante à redéfinir 
 
+//-- librairies standards --//
+#include <math.h>						// pour les différentes fonctions mathématique 
 
 //-- appel de headerfile  -> vos fichier headerfile 
 
- 
+//-- prototypes - WARNING à évité ici 
 
-//-- prototype - WARNING à évité ici 
-
-
-//-- variable globale - WARNING à évité 
-
-
-//-- CONSTANTE - WARNING à évité ici 
-
-
+//-- variables globales - WARNING à évité 
 
 //-- déclaration de fonction 
 
 
-//-- fonction principale -> main 
-// type_de_sortie nom_fct (param_entree1, param_entree2, ....)
+/* ----------------------------------------------------------------------------------
+// -> NOM FCT					:	main 
+// -> PARAMETRES ENTRES			:	-
+// -> PARAMETRE SORTIE			:   -
+// -> PARAMETRE IN/OUT -> ptr	:   - 
+// -> description				:	fonction principale -> début de programme 
+ ----------------------------------------------------------------------------------*/
 void main() 
 {   
 	//-- déclaration de variable --// 
-	// type nom_variable 
-	//type entier 
-	int longueur_m = 100, largeur_m = 10 ; 
+	//-- type entier 
+	int longueur_m = 100, largeur_m = 10, rayon; 
 
-	//type reel 
+	//-- type reel 
 	float surfaceCarre_m2, surfaceRectangle_m2, surfaceTriangle_m2, surfaceCercle_m2; 
 
-
-
-
-
-	//instruction 1
-
-	//instruction 2
-
-	// test condtion 
-		// instruction 1A
-
-
-		// instruction 2A
-
-	//instruction 3
-
+	//-- calculs --// 
+	//-- carré 
+	surfaceCarre_m2 = longueur_m ^ 2 ; 
+	//-- rectangle 
+    longueur_m * largeur_m = surfaceRectangle_m2; 
+	
+	//-- triangle 
+	surfaceTriangle_m2 = longueur_m * largeur_m / 2; 
+	
+	//-- cercle 
+	surfaceCercle_m2 = M_PI * rayon * rayon; 
 
 }
+
+//-- déclaration de fonction 
+
+
+
+
 	
 
 
