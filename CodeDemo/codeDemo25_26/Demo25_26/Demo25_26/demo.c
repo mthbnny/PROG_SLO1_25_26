ef054be3825483212e8767599ea2f4413dd62db6
user <user@etml-es.ch>
2025-11-03 16:41:03 +0100

    demo chapitre 3 et 4 -> headerFile + variable

--- CodeDemo/codeDemo25_26/Demo25_26/Demo25_26/demo.c
+++ CodeDemo/codeDemo25_26/Demo25_26/Demo25_26/demo.c
@@ -15,8 +15,11 @@
 //----------------------------------------------------------------------------------//
 //-- librairie standard --// 
 #include <stdio.h>
+#include <stdint.h>
 
 //-- librairie perso --//  
+#include "Conversion.h"
+
 
 
 //-- définition --// 
@@ -24,6 +27,8 @@
 #define PI 3.14 
 
 //-- constante gloable --// 
+
+
 
 
 //----------------------------------------------------------------------------------//
@@ -44,12 +49,41 @@ void main()
 
 
 	//-- variables --//
-	//-- 
+	//--- Entier Standard 
+	//--- Signé (+/-)
+	char varI;		// 1 octet
+	short varJ;		// 2 octets 
+	int varK;			// 4 octets			int = long 
+	long long varL;	// 8 octets 
+
+	//--- Non signé (+) 
+	unsigned char varM;		// 1 octet
+	unsigned short varN;	// 2 octets 
+	unsigned int varO;		// 4 octets			int = long 
+	unsigned long long varP;	// 8 octets 
+
+
+	//--- Entier Notrmalisé -> librairie stdint.h 
+		//--- Signé (+/-)
+	int8_t varA;		// 1 octet
+	int16_t varB;		// 2 octets 
+	int32_t varC;			// 4 octets			int = long 
+	int64_t varD;	// 8 octets 
+
+	//--- Non signé (+) 
+	uint8_t varE;		// 1 octet
+	uint16_t varF;		// 2 octets 
+	uint32_t varG;			// 4 octets			int = long 
+	uint64_t varH;	// 8 octets 
+					
+	//--- Reel 
+	float varQ; 				// 4 octets	
+	double varR;				// 8 octets 
 
 	//-- Reel
 	float rayon_m = 10;          // _m => metre 
 								 // cast implcite -> entier -> reel
-	float perimetre1_m, perimetre2_m;
+	float perimetre1_m, perimetre2_m, perimetre3_m ;
 
 
 
@@ -60,11 +94,21 @@ void main()
 	//-- attention au cast implicite
 	perimetre2_m = (float)(FOIX_2 * PI * rayon_m);
 
+
 	//-- appel de fct 
+	//--> calcul perimetre ccercle 
+	perimetre3_m = CalculPerimetreCercle(rayon_m);
+
+
 	//--> printf 
-	printf("%f \r\n %f", perimetre1_m, perimetre2_m);
+	printf("%f \r\n %f \n %f", perimetre1_m, perimetre2_m, perimetre3_m);
 	
 }
+
+
+//-- fonction --- 
+
+
 
 
 
